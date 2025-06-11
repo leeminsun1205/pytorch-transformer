8c0193b1c22769db8d8a9548e8595e8d46b7a0ed
leeminsun1205 <minhnhut12052004@gmai.com>
2025-06-11 16:24:48 +0700

    update

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ def get_config():
         "lr": 10**-4,
         "seq_len": 100,
         "d_model": 512,
+        "root_path": "/kaggle/input/bt4-22520195/pytorch-transformer",
         "datasource": 'harouzie/vi_en-translation',
         "lang_src": "English",
         "lang_tgt": "Vietnamese",
@@ -20,7 +21,7 @@ def get_config():
 def get_weights_file_path(config, epoch: str):
     model_folder = f"{config['datasource']}_{config['model_folder']}"
     model_filename = f"{config['model_basename']}{epoch}.pt"
-    return str(Path('.') / model_folder / model_filename)
+    return str(Path(config['root_path']) / model_folder / model_filename)
 
 # Find the latest weights file in the weights folder
 def latest_weights_file_path(config):
